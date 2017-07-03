e7f6a05fcab627a6b6c580b8025765d5c0ac0af8
johnnyGoo <157781745@qq.com>
2017-07-03 19:26:49 +0800

    update

--- src/VuePopup.vue
+++ src/VuePopup.vue
@@ -36,7 +36,7 @@
 
     .vue-popup > .vue-popup-scroll > .vue-popup-panel > .content {
         padding: 10px;
-        box-sizing:border-box;
+        box-sizing: border-box;
 
     }
 
@@ -79,6 +79,13 @@
         outline: none
     }
 
+    .t300 {
+        transition: all 300ms;
+        -webkit-transition: all 300ms;
+        -moz-transition: all 300ms;
+        -ms-transition: all 300ms;
+        -o-transition: all 300ms;
+    }
 </style>
 
 <template>
@@ -86,15 +93,16 @@
         <div :style="{background:maskBackground}" :transition="mask_transition" class="vue-popup-mask"
              v-show="show"></div>
         <div class="vue-popup-scroll" @click="maskClick()" v-show="show" :transition="panel_transition">
-            <div class="vue-popup-panel" :class="{'vue-popup-shake-animation':shake==true}"
+            <div class="vue-popup-panel t300" :class="{'vue-popup-shake-animation':shake==true}"
                  @click="panelClick()" :style="popupPanelStyle">
                 <div :style="titleStyle"
-                     class="vue-popup-title" v-show="title!=''" >{{title}}
+                     class="vue-popup-title" v-show="title!=''">{{title}}
                 </div>
                 <div class="close-button" v-show="closeButton" @click="close()">
                     <svg version="1.1" id="vue-popup-title-close" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="12px" height="12px" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve"><path d="M507.331,411.33c-0.002-0.002-0.004-0.004-0.006-0.005L352.003,256l155.322-155.325c0.002-0.002,0.004-0.003,0.006-0.005c1.672-1.673,2.881-3.627,3.656-5.708c2.123-5.688,0.912-12.341-3.662-16.915L433.952,4.674c-4.574-4.573-11.226-5.783-16.914-3.66c-2.08,0.775-4.035,1.984-5.709,3.655c0,0.002-0.002,0.003-0.004,0.005L256.001,160L100.677,4.675c-0.002-0.002-0.003-0.003-0.005-0.005c-1.673-1.671-3.627-2.88-5.707-3.655c-5.69-2.124-12.341-0.913-16.915,3.66L4.676,78.049c-4.574,4.574-5.784,11.226-3.661,16.914c0.776,2.08,1.985,4.036,3.656,5.708c0.002,0.001,0.003,0.003,0.005,0.005L160.001,256L4.676,411.326c-0.001,0.002-0.003,0.003-0.004,0.005c-1.671,1.673-2.88,3.627-3.657,5.707c-2.124,5.688-0.913,12.341,3.661,16.915l73.374,73.373c4.575,4.574,11.226,5.784,16.915,3.661c2.08-0.776,4.035-1.985,5.708-3.656c0.001-0.002,0.003-0.003,0.005-0.005l155.324-155.325l155.324,155.325c0.002,0.001,0.004,0.003,0.006,0.004c1.674,1.672,3.627,2.881,5.707,3.657c5.689,2.123,12.342,0.913,16.914-3.661l73.373-73.374c4.574-4.574,5.785-11.228,3.662-16.915C510.212,414.957,509.003,413.003,507.331,411.33z" fill="{{closeColor}}"/> </svg>
                 </div>
-                <div class="content" :style="{maxHeight:maxHeight,overflow:'auto',height:size.height+'px',width:size.width+'px'}">
+                <div class="content t300"
+                     :style="{maxHeight:(maxHeight>0?(maxHeight+'px'):''),overflow:'auto',height:size.height+'px',width:size.width+'px'}">
                     <slot>{{{content}}}</slot>
                 </div>
                 <div class="vue-popup-footer" v-show="footer">
@@ -113,7 +121,7 @@
 
 
 /*!
-* vue-popup v0.0.1 (https://github.com/johnnyGoo/vue-popup)
+* vue-popup v0.0.2(https://github.com/johnnyGoo/vue-popup)
 * Author: Johnny chen
 *
 * Copyright 2013-2015 Johnny chen
@@ -141,8 +149,8 @@
     var shake_class_name = 'vue-popup-shake-animation';
 
 
-//
-    var anim = Smart.Animations.create('vue-popup-shake-animation--keyframe',{
+    //
+    var anim = Smart.Animations.create('vue-popup-shake-animation--keyframe', {
         '10%, 90%': {transform: 'translate3d(-2px, 0, 0)'},
         '20%, 80%': {transform: 'translate3d(4px, 0, 0)'},
         '30%, 50%, 70%': {transform: 'translate3d(-8px, 0, 0)'},
@@ -215,7 +223,7 @@
             },
             popupPanelStyle: {
                 type: Object, default: function () {
-                    return {background:'#fff',boxShadow:'0 2px 3px #666',width:'400px'}
+                    return {background: '#fff', boxShadow: '0 2px 3px #666', width: '400px'}
                 }
             },
             cancelStyle: {
@@ -231,13 +239,13 @@
 
             maskBackground: {type: String, default: "rgba(0, 0, 0, 0.6)"},
             maxHeight: {
-                type: String,
-                default: ''
+                type: Number,
+                default: -1
             },
-            size:{
-                type:Object,
-                default:function () {
-                    return {width:400,height:300};
+            size: {
+                type: Object,
+                default: function () {
+                    return {width: 500, height: 400};
                 }
             }
         },
@@ -291,7 +299,9 @@
                         width: this.$el.querySelector('.vue-popup-panel').clientWidth
                     };
 
-                   // popSize=this.size;
+
+
+                    popSize = this.size;
 
 
                     var windowSize = {
@@ -300,10 +310,21 @@
                     };
                     popSize.width = parseInt(popSize.width);
 
-                    var css = {'margin-top': 0, 'overflow-x': 'hidden'};
+                    var css = {'margin-top': 0, 'overflow': 'hidden'};
                     //css['width'] = popSize.width;
-                    if (popSize.height < windowSize.height - 60) {
-                        css['margin-top'] = (windowSize.height - popSize.height) / 2;
+
+                    var ph = popSize.height
+                    if (this.maxHeight && this.maxHeight > 0) {
+                        ph = Math.min(ph, this.maxHeight);
+                    }
+
+
+                    if (ph < windowSize.height - 60) {
+
+
+                        css['margin-top'] = (windowSize.height - ph) / 2;
+
+
                         css['margin-bottom'] = 30
                     } else {
                         css['margin-top'] = 30;
@@ -359,7 +380,7 @@
             }
 
         }, ready: function () {
-            this.popupPanelStyle.width=this.size.width+'px';
+            this.popupPanelStyle.width = this.size.width + 'px';
             if (this.show) {
                 this.show = false;
                 var me = this;
@@ -389,12 +410,11 @@
                     this.removeResize();
                 }
             },
-            'size.width':function (val,oldVal) {
-                this.popupPanelStyle.width=parseInt(val)+'px';
-                console.log( this.popupPanelStyle)
+            'size.width': function (val, oldVal) {
+                this.popupPanelStyle.width = Math.ceil(val) + 'px';
                 this.updateSize();
             },
-            'size.height':function (val,oldVal) {
+            'size.height': function (val, oldVal) {
                 this.updateSize();
             }
         }
